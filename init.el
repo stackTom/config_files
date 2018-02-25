419b34c51bd63538f6b07211050dfa0f4c7bd7c3
stackTom <rtiky23@gmail.com>
2018-02-25 19:45:31 +0000

    add my emacs init.el

---
+++ init.el
@@ -0,0 +1,22 @@
+;; I hate tabs!
+(setq-default indent-tabs-mode nil)
+(global-linum-mode t)
+(require 'whitespace)
+;;adjust this value to change column (AKA # of characters) at which text will be highlighted
+(setq whitespace-line-column 80)
+(setq whitespace-style '(face empty tabs lines-tail trailing))
+(global-whitespace-mode t)
+;; color themes
+(add-to-list 'load-path "/path/to/color-theme.el/file")
+(require 'color-theme)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-hober)))
+;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
+(custom-set-variables
+  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
