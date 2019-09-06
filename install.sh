d3a0e7f0ffb222bed945ac4fb67aca7471bb1049
stackTom <rtiky23@gmail.com>
2019-09-06 19:29:01 -0400

    create ~/.emacs.d/ if not there

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     cp ./.vimrc ~/.xvimrc
 fi
 cp ./.tmux.conf ~/
+if [ ! -d ~/.emacs.d/ ]; then
+    mkdir ~/.emacs.d/
+fi
 cp ./init.el ~/.emacs.d/
 
 if ! grep -q "set -o vi" ~/.bashrc; then
